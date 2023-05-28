507c2bb62955f10b351abb94b2aec2a0d2a2d2ab
Trey Marshall <marshall.trey92@yahoo.com>
2023-05-28 12:51:43 -0500

    Add PR template, base component files, initial small TypeScript changes.

---
+++ src/components/App/App.tsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+import apiCalls from '../../apiCalls'
+
+apiCalls()
+
+export class App extends Component<{}, {}> {
+  constructor(name: string) {
+    super(name)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src="" className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+    )
+  }
+}
+
+export default App;

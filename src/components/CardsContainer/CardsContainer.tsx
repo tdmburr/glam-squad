98d0ae4f2ae3ac1d5a522a745dddec61896a1d1e
Devynne Marshall <devynnem@gmail.com>
2023-05-28 14:10:32 -0600

    feat:Add some mapping over CardsContainer

--- src/components/CardsContainer/CardsContainer.tsx
+++ src/components/CardsContainer/CardsContainer.tsx
@@ -1,11 +1,22 @@
 // name, brand, price, image
+import SingleCard from '../SingleCard/SingleCard' 
 import './CardsContainer.css'
 
-const CardsContainer = () => {
+const CardsContainer = (props) => {
+  const card = props.map(prop => {
+    return (
+      <div className='cards-container'>
+        <SingleCard 
+          key={prop.id} 
+          
+          />
+        
+      </div>
+    )
+  })
   return (
-    <div className='cards-container'>
-      <img src=''/>
-      
+    <div>
+      {card}
     </div>
   )
 }

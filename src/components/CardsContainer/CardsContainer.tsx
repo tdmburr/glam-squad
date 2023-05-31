646c1fb8a7852f804d1b301197b61a9ce92faa82
Rachel Brendel <brendelrachel@gmail.com>
2023-05-31 13:35:57 -0500

    feat/ basic css set up git status

--- src/components/CardsContainer/CardsContainer.tsx
+++ src/components/CardsContainer/CardsContainer.tsx
@@ -1,25 +1,25 @@
-// name, brand, price, image
-import SingleCard from '../SingleCard/SingleCard' 
-import './CardsContainer.css'
-
-const CardsContainer = (props) => {
-  const card = props.map(prop => {
-    return (
-      <div className='cards-container'>
-        <SingleCard 
-          key={prop.id} 
+// // name, brand, price, image
+// import SingleCard from '../SingleCard/SingleCard' 
+// import './CardsContainer.css'
+const CardsContainer = ""
+// const CardsContainer = (props) => {
+//   const card = props.map(prop => {
+//     return (
+//       <div className='cards-container'>
+//         <SingleCard 
+//           key={prop.id} 
           
-          />
+//           />
         
-      </div>
-    )
-  })
-  return (
-    <div>
-      {card}
-    </div>
-  )
-}
+//       </div>
+//     )
+//   })
+//   return (
+//     <div>
+//       {card}
+//     </div>
+//   )
+// }
 
 
 export default CardsContainer
--- src/components/SingleCard/SingleCard.tsx
+++ src/components/SingleCard/SingleCard.tsx
@@ -1,15 +1,15 @@
-import data from '../../mockData.js'
-import './SingleCard.css'
-
-const SingleCard = () => {
-  return (
-    <li key={card.id}>
-      <img src={card.image_link} alt={card.name} />
-      <h2>{card.name}</h2>
-      <p>{card.brand}</p>
-      <p>{card.price}</p>
-    </li>
-  )
-}
+// import data from '../../mockData.js'
+// import './SingleCard.css'
+ const SingleCard = ""
+// const SingleCard = () => {
+//   return (
+//     <li key={card.id}>
+//       <img src={card.image_link} alt={card.name} />
+//       <h2>{card.name}</h2>
+//       <p>{card.brand}</p>
+//       <p>{card.price}</p>
+//     </li>
+//   )
+// }
 
 export default SingleCard

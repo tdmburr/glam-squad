878a53480be86ffe635ab35272609914c936dc2f
Trey Marshall <marshall.trey92@yahoo.com>
2023-06-03 14:48:17 -0500

    feat: Add more Cypress testing of user flows - add error redirect to app

--- cypress/e2e/dashboard_spec.cy.ts
+++ cypress/e2e/dashboard_spec.cy.ts
@@ -4,13 +4,13 @@ describe('User dashboard with user flows.', () => {
   
   beforeEach(() => {
     
-    cy.intercept("GET", 'http://makeup-api.herokuapp.com/api/v1/**', {
+    cy.intercept("GET", 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=CoverGirl', {
       statusCode: 200,
       fixture: 'multipleStub.json'
     })
-    cy.intercept("GET", 'http://makeup-api.herokuapp.com/api/v1/', {
+    cy.intercept("GET", 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline', {
       statusCode: 200,
-      fixture: 'singleStub.json'
+      fixture: 'multipleStub.json'
     })
     cy.visit("http://localhost:3000/")   
   })
@@ -34,9 +34,29 @@ describe('User dashboard with user flows.', () => {
       .contains('h3', "Featured Brand: Maybelline")     
   })
 
-  it('Should have a featured brand tag to display to the user', () => {
+  it('Should have a featured brand tag to display to the user by default', () => {
 
     cy.get('.featured-container')
       .contains('h3', "Featured Brand: CoverGirl")
   })
+
+  it('Should have visible cards for a brand, they should be clickable to view product details, following being clicked, you should be able to click the logo to return home', () => {
+
+     cy.get('[href="/maybelline/495"] > .product-card > .product-name')
+      .click()
+    cy.get('.product-info-img')
+    cy.get('.product-name')
+    cy.get('.product-info-container')
+      .contains('Brand')
+    cy.get('.product-info-container')
+      .contains('Price')
+    cy.get('.product-info-container')
+      .contains('Rating')  
+    cy.get(':nth-child(2) > :nth-child(6)')
+      .contains('Maybelline Face Studio Master Hi-Light Light Boosting bronzer formula has an expert balance of shade + shimmer illuminator')
+    cy.get('#header-logo')
+      .click()
+  })
+
+
 })
